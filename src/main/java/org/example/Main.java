e56972761fe65a730b7da6695fa0c461b98c0886
ulka <ulka@ulka.com>
2023-06-13 14:54:04 +0200

    1st commit of gradle-hometask

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.gson.Gson;
+import org.example.dto.PersonDto;
+
+public class Main {
+    public static void main(String[] args) {
+        PersonDto person = new PersonDto("Yulia", "Zablotska");
+        System.out.println(new Gson().toJson(person));
+
+    }
+}
